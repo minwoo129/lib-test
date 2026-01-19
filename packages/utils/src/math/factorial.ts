6bb8f9d27bcd16794d47ac6640c100eeca91a6b9
minwoo129 <key0129mw@naver.com>
2026-01-19 12:57:41 +0900

    feat: #6 factorial 함수에 음수 처리 추가

--- packages/utils/src/math/factorial.ts
+++ packages/utils/src/math/factorial.ts
@@ -1,5 +1,8 @@
 export const factorial = (num: number): number => {
-  if (num === 1) return 1;
+  if (num < 0) {
+    throw new Error('Factorial is not defined for negative numbers');
+  }
+  if (num === 0 || num === 1) return 1;
 
   return num * factorial(num - 1);
 };
